54934f969e824039d8f31286014a828c1e947402
amccormack <79875785+adammccormack@users.noreply.github.com>
2023-01-19 23:45:11 +0000

    added mock api

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 
 const port = 4567;
 
+const dogs = [
+  { name: "Fred", breed: "Dalmatian" },
+  { name: "Bob", breed: "German Shepherd" },
+];
+
 app.get("/", (req, res) => {
-  res.send("HEYOOOOO");
+  res.json(dogs);
 });
 
-app.get("/about", (req, res) => {
-  res.send("What would you like to know about?");
+app.get("/dogs/:id", (req, res) => {
+  res.json(dogs[parseInt(req.params.id) - 1]);
 });
 
 app.listen(port, () => {
